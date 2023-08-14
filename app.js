71b5aec4fd4b3f41bbd1066011f3c8449336c3d1
DHughes666 <graziemolto6@gmail.com>
2023-08-14 02:23:48 +0100

    Next level - Password hashing

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+const md5 = require("md5");
+
 const mongoose = require("mongoose");
-const uniqueValidator = require('mongoose-unique-validator');
-const encrypt = require('mongoose-encryption');
+
 mongoose.connect('mongodb://localhost:27017/AuthenticateDB', {
   useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => {
@@ -32,13 +33,6 @@ const userSchema = new mongoose.Schema ({
   }
 });
 
-//Apply the unique validator to the schema
-// userSchema.plugin(uniqueValidator);
-
-
-// Create a secret
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']})
 
 
 //Now we can create the schema
@@ -61,7 +55,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save()
@@ -72,7 +66,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username})
   .then(foundUser => {
