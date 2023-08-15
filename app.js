829f06a48b69078ca39bfeb9eec61e502b2dc64e
DHughes666 <graziemolto6@gmail.com>
2023-08-15 01:11:47 +0100

    Phase 1 of google-auth implemented successfully

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -48,6 +50,7 @@ const userSchema = new mongoose.Schema ({
 
 //Passport will hash, sort and organize passwords into our database
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate)
 
 //Now we can create the schema
 const User = new mongoose.model('User', userSchema);
@@ -57,6 +60,19 @@ passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 // const user = new DefaultUser({username: 'user'});
 // await user.setPassword('password');
@@ -68,6 +84,13 @@ const context = 'Harry Loraine!! I hope that makes sense to you?'
 app.get("/", function(req, res){
   res.render("home");
 });
+
+// app.get("/auth/google", function(req, res){
+//   passport.authenticate("google", {scope: ["profile"]})
+// })
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
 
 app.get("/login", function(req, res){
   res.render("login");
